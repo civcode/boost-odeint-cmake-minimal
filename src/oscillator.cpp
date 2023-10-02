e5cc18b7ee19cbad168e51e81ca3285c50866440
chris <ivi3001@yahoo.de>
2023-10-02 15:48:30 +0200

    added integrate() with static and non-static member function via bind

--- src/oscillator.cpp
+++ src/oscillator.cpp
@@ -10,9 +10,9 @@
 
 using namespace std;
 using namespace boost::numeric::odeint;
+using namespace std::placeholders;
 
 typedef std::vector< double > state_type;
-
 
 /* The rhs of x' = f(x) defined as a class */
 class harm_osc {
@@ -28,25 +28,43 @@ public:
         dxdt[0] = x[1];
         dxdt[1] = -x[0] - m_gam*x[1];
     }
-    static void func ( const state_type &x , state_type &dxdt , const double /* t */ )
+    static void func_static ( const state_type &x , state_type &dxdt , const double /* t */ )
     {
         double m_gam = 1.0;
-        
         dxdt[0] = x[1];
         dxdt[1] = -x[0] - m_gam*x[1];
-    }    
+    }
+
+    void func_non_static ( const state_type &x , state_type &dxdt , const double /* t */ )
+    {
+        m_gam = 1.0;
+        dxdt[0] = x[1];
+        dxdt[1] = -x[0] - m_gam*x[1];
+    }
+
     // static void wrapper(const state_type &x , state_type &dxdt , const double t) {
 
     //   this->operator(x, dxdt, t);
     // }
 
-    void run() {
+    void run_static() {
       state_type x = {0.10 , 0.0}; // initial conditions
       state_type y = {0.10 , 0.0}; // initial conditions
       //size_t step_count = integrate(this->operator(), x, 0.0, 5.0, 0.01, write_lorenz);
-      size_t step_count = integrate(this->func, x, 0.0, 5.0, 0.01, write_lorenz);
+      size_t step_count = integrate(this->func_static, x, 0.0, 5.0, 0.01, write_lorenz);
+      //size_t step_count = integrate(std::bind(&harm_osc::func_non_static, this, _1, _2, _3), x, 0.0, 5.0, 0.01, write_lorenz);
 
     }
+
+    void run_non_static() {
+      state_type x = {0.10 , 0.0}; // initial conditions
+      state_type y = {0.10 , 0.0}; // initial conditions
+      //size_t step_count = integrate(this->operator(), x, 0.0, 5.0, 0.01, write_lorenz);
+      //size_t step_count = integrate(this->func, x, 0.0, 5.0, 0.01, write_lorenz);
+      size_t step_count = integrate(std::bind(&harm_osc::func_non_static, this, _1, _2, _3), x, 0.0, 5.0, 0.01, write_lorenz);
+
+    }
+
     static void write_lorenz(const state_type &x , const double t)
     {
       //cout << std::setw(3);
@@ -118,6 +136,11 @@ int main(int argc, char **argv)
 
   cout << "static" << endl;
   timer.tic();
-  osc.run();
+  osc.run_static();
+  cout << "time expired: " << timer.toc().ms().to_string() << endl;
+
+  cout << "non-static" << endl;
+  timer.tic();
+  osc.run_non_static();
   cout << "time expired: " << timer.toc().ms().to_string() << endl;
 }
